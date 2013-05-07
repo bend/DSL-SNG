631c1fdc7cfba12458b40fa94b0ed6a69500ef48
Christopher Castermane <christopher.castermane@gmail.com>
2013-05-07 18:30:32 +0200

    cool

---
+++ src/scenarios/Influence.scala
@@ -0,0 +1,20 @@
+package scenarios
+
+import scala.util.Random
+import scala.math
+import api.Person
+
+/**
+ * If the user has many friends, and is young, he will more likely want to join the social network
+ */
+class Influence extends Scenario {
+	name = "Influence"
+	
+	override def simulate(p: Person, day:Int) : (Float, Float) = {
+	  var chance_join = 0f
+	  
+	  chance_join = p.nbr_relations * (1f/(p.age/36.5f))
+	  
+	  return(chance_join,0f)
+	}
+}
